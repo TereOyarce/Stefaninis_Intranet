1c532f626ad28980b0ff337aa805fe7038f2e10e
Marianny Garcia <mdvgarciar@gmail.com>
2021-10-22 11:42:44 -0300

    components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import './style/home.css';
 
 
 export const Home= () =>{
   return(
-    <div>
+    <div className="container-home">
       <h1>WELCOME!!!</h1>
     </div>
   )
---
+++ src/components/Vancancies.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './style/vacancies.css';
+
+
+export const Vacancies= () =>{
+  return(
+    <div className="container-vacancies">
+      <h1>THIS IS VACANCIES</h1>
+     
+    </div>
+  )
+}
