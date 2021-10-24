4d3c0113c29f4449069bf01f08b16182395ea381
TeresaOyarce <t.oyarce05@gmail.com>
2021-10-23 22:36:10 -0300

    profile design ok

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,29 +2,32 @@ import React from "react";
 import './style/profile.css';
 import profileDefault from '../assets/img/profiledefault.png'
 
-export const Profile= () =>{
-  return(
+
+export const Profile = () => {
+
+  return (
     <div className="container-profile">
 
       <h1>Mi Perfil</h1>
       <img src={profileDefault} alt="profile image" className='profile-img' />
-     <form >
-       <input type="text" name="name" placeholder='Nombre' />
-       <input type="text" name="rut" placeholder='Rut' />
-       <input type="text" name="gender" placeholder='Género' />
-       <input type="text" name="birthday" placeholder='Fecha de Nacimiento' />
-       <input type="text" name="address" placeholder='Dirección' />
-       <input type="text" name="phone" placeholder='Teléfono' />
-       <input type="text" name="familiar" placeholder='Carga Familiar' />
-      <select name="t-shirt" id="t-shirt">
-        <option value="">Talla de Polera</option>
-        <option value="s">S</option>
-        <option value="m">M</option>
-        <option value="l">L</option>
-        <option value="xl">XL</option>
-        <option value="xxl">XXL</option>
-      </select>
-     </form>
+      <form >
+        <input type="text" className='input-profile' name="name" placeholder='Nombre' />
+        <input type="text" className='input-profile' name="rut" placeholder='Rut' />
+        <input type="text" className='input-profile' name="gender" placeholder='Género' />
+        <input type="text" className='input-profile' name="birthday" placeholder='Fecha de Nacimiento' />
+        <input type="text" className='input-profile' name="address" placeholder='Dirección' />
+        <input type="text" className='input-profile' name="phone" placeholder='Teléfono' />
+        <input type="text" className='input-profile' name="familiar" placeholder='Carga Familiar' />
+        <select name="t-shirt" id="t-shirt">
+          <option value="">Talla de Polera</option>
+          <option value="s">S</option>
+          <option value="m">M</option>
+          <option value="l">L</option>
+          <option value="xl">XL</option>
+          <option value="xxl">XXL</option>
+        </select>
+      </form>
+      <button className='profile-button'>GUARDAR</button>
     </div>
   )
 }
