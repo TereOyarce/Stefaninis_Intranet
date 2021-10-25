7ae0ca8c6f50a5bdf84ed8db9398026c09c0dcd6
Marianny Garcia <mdvgarciar@gmail.com>
2021-10-24 22:15:49 -0300

    bars

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,21 +4,17 @@ import './style/sidebar.css';
 import { Icon } from '@iconify/react';
 import logo from '../assets/img/logo.png';
 import profileImg from '../assets/img/profiledefault.png';
-
 export const Sidebar= () =>{
-
   return(
     <div className="container-sidebar">
-      <div className="container-logo"> 
+<div className="container-logo"> 
         <Link to='/'>
           <img src={logo} alt="Stefanini" className="logo-navbar"/>
         </Link>
       </div>
-
       <div className ='img-container'>
         <img src={profileImg} alt="profile default" className='img-profile' />
       </div>
-
       <div className='links-sidebar'>
       <Link to='/profile'>
           <button className="category-2">PERFIL</button>
@@ -35,6 +31,7 @@ export const Sidebar= () =>{
             CERRAR SESIÓN
             <Icon icon="bx:bx-log-out"  height="15" rotate={2} hFlip={true} vFlip={true} className="log-icon" />
           </button>
+          
       </Link>
       </div>
     </div>
