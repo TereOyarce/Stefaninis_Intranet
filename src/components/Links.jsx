8af417460bc61b5f824d7ffa673ce48f1db80ed1
MaJesus <mareyes.esp@gmail.com>
2021-10-26 20:53:57 -0300

    Cards overlay fixed

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -12,44 +12,51 @@ export const Links= () =>{
       </div>
 
       <div className='body-links'>
-      <a target='_blank' href='https://stefanini.konviva.com.br/lms/#/home'>
-      <div className='card-container1'>
-        <div className='overlay'>
+
+     
+      <div className='card-container'>
+         <a target='_blank' href='https://stefanini.konviva.com.br/lms/#/home'>
+           <div className='overlay'>
             <h1>Academia</h1>
             <p>Plataforma de Aprendizaje Corporativo impartido por Stefaniners para Stefaniners.</p>
         </div>
         <div className='title-box'>
             <h2>ACADEMIA</h2>
-        </div>
+        </div> 
+        </a>
       </div>
-      </a>
+     
 
-      <a target='_blank' href='https://hr.acsendo.com/results/admin'>
-      <div className='card-container2'>
-        <div className='overlay'>
+     
+      <div className='card-container'>
+         <a target='_blank' href='https://hr.acsendo.com/results/admin'>
+           <div className='overlay'>
             <h1>Ascendo</h1>
             <p>Somos una plataforma integral de Recursos Humanos para evaluar el desempeño, aumentar el engagement y desarrollar las habilidades de los colaboradores.</p>
         </div>
         <div className='title-box'>
             <h2>ASCENDO</h2>
         </div>
-        </div>
         </a>
+        </div>
+        
 
-      <a target='_blank' href='https://mentoring.stefanini.io/#/login'>
-      <div className='card-container3'>
-        <div className='overlay'>
+      
+      <div className='card-container'>
+        <a target='_blank' href='https://mentoring.stefanini.io/#/login'>
+          <div className='overlay'>
             <h1>Mentoring</h1>
             <p>Plataforma que permite a todos los colaboradores la transmisión informal del conocimiento a través de mentorías, que suman al desarrollo profesional de quien lo recibe.</p>
         </div>
         <div className='title-box'>
             <h2>MENTORING</h2>
-        </div>
+        </div> 
+        </a>
       </div>
-      </a>
 
-      <a target='_blank' href='https://integra.stefanini.com/portal/0103PROD/PortalSkillMapping#'>
-      <div className='card-container4'>
+      
+      <div className='card-container'>
+        <a target='_blank' href='https://integra.stefanini.com/portal/0103PROD/PortalSkillMapping#'>
         <div className='overlay'>
             <h1>Skill Mapping</h1>
             <p>Nuestro Mapa de Talento LATAM.</p>
@@ -57,20 +64,21 @@ export const Links= () =>{
         <div className='title-box'>
             <h2>SKILL MAPPING</h2>
         </div>
+        </a>
       </div>
-      </a>
-
-      <a target='_blank' href='https://web.yammer.com/main/feed'>
-      <div className='card-container5'>
+      
+      <div className='card-container'>
+        <a target='_blank' href='https://web.yammer.com/main/feed'>
         <div className='overlay'>
             <h1>Yammer</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         </div>
         <div className='title-box'>
             <h2>YAMMER</h2>
-        </div>
+        </div> 
+        </a>
       </div>
-      </a>
+     
     
       </div>
 
