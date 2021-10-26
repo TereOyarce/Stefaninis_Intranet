8987ee12f070730183b9af44fb4d0d4447dd51bf
Marianny Garcia <mdvgarciar@gmail.com>
2021-10-26 14:20:30 -0300

    pdf

--- src/components/Insurance.jsx
+++ src/components/Insurance.jsx
@@ -1,25 +1,28 @@
 import React from "react";
 import './style/insurance.css';
 import insurance from '../assets/img/insurance.jpg';
-import { Icon } from '@iconify/react';
 import logovc from '../assets/img/logovc.png';
 import insurance2 from '../assets/img/insurance2.jpg';
 import Video from './Video'
+import Pdf from "../assets/pdf/folleto.pdf";
+import Pdf2 from "../assets/pdf/manual.pdf"
 
 
 export const Insurance= () =>{
 
-  const general = () => {document.querySelector('.general-insurance').style.display='block';
+  const general = () => {document.querySelector('.general-insurance').style.display='';
   document.querySelector('.manual-insurance').style.display='none';
   document.querySelector('.refund-insurance').style.display='none';}
 
-  const manual = () => {document.querySelector('.manual-insurance').style.display='block';
+  const manual = () => {document.querySelector('.manual-insurance').style.display='';
   document.querySelector('.general-insurance').style.display='none';
   document.querySelector('.refund-insurance').style.display='none';}
 
-  const refund = () => {document.querySelector('.refund-insurance').style.display='block';
+  const refund = () => {document.querySelector('.refund-insurance').style.display='';
   document.querySelector('.general-insurance').style.display='none';
   document.querySelector('.manual-insurance').style.display='none';}
+
+  
 
   return(
     <div className='containter-insurance'>
@@ -35,7 +38,7 @@ export const Insurance= () =>{
 
     <div className='body-insurance'>
     
-    <div className="general-insurance">
+    <div className="general-insurance" >
       <h3>SEGURO COMPLEMENTARIO</h3>
       <img src={insurance} alt="Seguro" className="img-insurance"/>
       <h4>Vigencia 01 de enero 2021 al 31 de diciembre de 2021.</h4>
@@ -47,19 +50,13 @@ export const Insurance= () =>{
       <li >Convenios y beneficios de salud</li>
       <button 
       className="btn-insurance"
-      onClick=""
-      >LEE MÁS</button>
+      onClick={()=>window.open(Pdf)}
+      >Lee más</button>
       <h5>Si aún tienes dudas sobre como leer tu seguro de salud</h5>
-      <button 
-      className="btn-insurance"
-      onClick=""
-      >
-      <Icon icon="bi:play-fill" color="#0b1641" height="60" rotate={2} hFlip={true} vFlip={true} />  
-      Mira este video</button>
-      {/* <Video /> */}
+      <Video ytId="m5NV2tYLEpo"/>
     </div>  
 
-    <div className="manual-insurance">
+    <div className="manual-insurance"style={{display: "none"}}>
       <h3>MANUAL DEL USUARIO</h3>
       <img src={insurance2} alt="Seguro" className="img-insurance"/>
       <div className="p-insurance"><p>Este manual tiene por objetivo entregarte información de cómo hacer uso
@@ -73,19 +70,23 @@ export const Insurance= () =>{
       acuerdo a las necesidades y requerimientos de cada organización que
       contrata a Vida Cámara, por eso, te invitamos a leer tu Plan de Reembolso
       y Póliza.</p></div>
-      <h4>Descarga tu manual acá:</h4>
+      <h4>Manual general para asegurados:</h4>
       <button 
       className="btn-insurance"
-      onClick=""
-      >DESCARGAR</button>
+      onClick={()=>window.open(Pdf2)}
+      >Lee más</button>
       <a href="https://www.vidacamara.cl/" target="_blank">
       <img src={logovc} alt="Logo-VC" className="logo-vc"/></a>
     </div>
 
-     <div className="refund-insurance">
+     <div className="refund-insurance"style={{display: "none"}}>
       <h3>REEMBOLSO</h3>
+      <div className="p-insurance"><p>Es importante que sepas que cada plan es diferente pues está hecho de
+      acuerdo a las necesidades y requerimientos de cada organización que
+      contrata a Vida Cámara, por eso, te invitamos a leer tu Plan de Reembolso
+      y Póliza</p></div>
       <h4>Aprende a usar la app de reembolso</h4>
-      < Video />
+      < Video ytId="fQ3VMNCk9Lw"/>
     </div>    
 
 
