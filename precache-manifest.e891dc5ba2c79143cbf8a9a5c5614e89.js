44f89c152d65c8dac641deb08d2da5b1ec017915
TeresaOyarce <t.oyarce05@gmail.com>
2021-10-27 10:25:30 -0300

    Updates

---
+++ precache-manifest.e891dc5ba2c79143cbf8a9a5c5614e89.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22705dcbf158a0561ce584e2fdaac8b6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "53249aa6ce75230b440f",
+    "url": "./static/css/2.fa5d8a67.chunk.css"
+  },
+  {
+    "revision": "c9137259bcf65ae5e82a",
+    "url": "./static/css/main.142b455d.chunk.css"
+  },
+  {
+    "revision": "53249aa6ce75230b440f",
+    "url": "./static/js/2.3297e47f.chunk.js"
+  },
+  {
+    "revision": "d8d8d15ddaa0fba847f5f67864e9bb68",
+    "url": "./static/js/2.3297e47f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9137259bcf65ae5e82a",
+    "url": "./static/js/main.d87e5032.chunk.js"
+  },
+  {
+    "revision": "d4baf75ae2885ee40b2e",
+    "url": "./static/js/runtime-main.2e86fcc6.js"
+  },
+  {
+    "revision": "4fb55f6dcb046972dcf1124ba977ec3a",
+    "url": "./static/media/folleto.4fb55f6d.pdf"
+  },
+  {
+    "revision": "f1f2a97acb207984efd4cf739fa7dd1d",
+    "url": "./static/media/infografia.f1f2a97a.pdf"
+  },
+  {
+    "revision": "4c5effeff2c7c1b1a6ce1b92b0aeff1c",
+    "url": "./static/media/insurance.4c5effef.jpg"
+  },
+  {
+    "revision": "60cdd48d3505726ddf83d7e425935f7e",
+    "url": "./static/media/insurance2.60cdd48d.jpg"
+  },
+  {
+    "revision": "b647f571db4c8f0f085f1e7f269ec5c2",
+    "url": "./static/media/insurance3.b647f571.jpg"
+  },
+  {
+    "revision": "eb45f3a8f5a9450136bf701f8b73ec54",
+    "url": "./static/media/logo.eb45f3a8.png"
+  },
+  {
+    "revision": "91a3179926c166b69de1f553c0d44cc6",
+    "url": "./static/media/logovc.91a31799.png"
+  },
+  {
+    "revision": "a030f2f9e77f3699baaf4ca767fa0303",
+    "url": "./static/media/manual.a030f2f9.pdf"
+  },
+  {
+    "revision": "3e5c87e08e6ef77e80448caf22940e98",
+    "url": "./static/media/profiledefault.3e5c87e0.png"
+  },
+  {
+    "revision": "f5ea117f0b96d499c61ae5acf582bad2",
+    "url": "./static/media/reembolso.f5ea117f.pdf"
+  }
+]);
